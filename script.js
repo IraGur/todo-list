e13519a31c999536ccf1aa4dd6b41913bd764f7d
Ira <116351544+IraGur@users.noreply.github.com>
2023-09-26 23:45:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let todos = [];
-
-var test = 1;
+const todos = [];
 
 while (true) {
-   let input = prompt(
+   const input = prompt(
       "Please make your choice :   \n 1. NEW: to add a todo \n 2. LIST: to see all the todos \n 3. DELETE: to remove a specific todo \n 4. QUIT: to exit de program "
    ).toLowerCase();
 
@@ -12,14 +10,15 @@ while (true) {
 
       if (!todos.length) {
          console.log("Todo list is empty");
+      }else{
+         for (let i = 0; i < todos.length; i++) {
+            const index = i + 1;
+            console.log(`${index}. ${todos[i]}`);
+         }
       }
 
-      for (let i = 0; i < todos.length; i++) {
-         const index = i + 1;
-         console.log(`${index}. ${todos[i]}`);
-      }
    } else if (input === "new") {
-      let newTodo = prompt("Enter new todo");
+      const newTodo = prompt("Enter new todo");
       todos.push(newTodo);
       console.log(`Item '${newTodo}' added to the list`);
    } else if (input === "delete") {
